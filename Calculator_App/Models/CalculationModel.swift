dc846f3b44659636f55de5de7941a44410ba7f6e
Николай <imkoddi@mail.ru>
2022-10-08 19:14:52 +0300

    Created OperationsModel and CalculationModel

---
+++ Calculator_App/Models/CalculationModel.swift
@@ -0,0 +1,16 @@
+//
+//  CalculationModel.swift
+//  Calculator_App
+//
+//  Created by KODDER on 08.10.2022.
+//
+
+import UIKit
+
+class CalculationModel {
+    
+    private var firstNumber = 0.0
+    private var secondNumber = 0.0
+    private var currentNumber = ""
+    private var currentOperation = Operations.noAction
+}
---
+++ Calculator_App/Models/OperationsModel.swift
@@ -0,0 +1,16 @@
+//
+//  OperationsModel.swift
+//  Calculator_App
+//
+//  Created by KODDER on 08.10.2022.
+//
+
+import UIKit
+
+enum Operations {
+    case noAction
+    case addition
+    case subtraction
+    case multiplication
+    case division
+}
